83bcf56313bc92a664c0741e0dd7f590d42ba518
Roman Loiko <grevzi@gmail.com>
2020-10-10 12:56:24 +0300

    - added Block Class for model

--- src/model.js
+++ src/model.js
@@ -1,55 +1,44 @@
 import image from './assets/image.png'
 import {Block} from './classes/blocks'
 export const model = [
-    {
-        type: 'title', value: 'Site Constructor on Vanilla JS', options: {
-            tag: 'h2',
-            styles: {
-                background: 'linear-gradient(to right, #ff0099, #493240)',
-                color: '#ffffff',
-                'text-align': 'center',
-                padding: '0.25rem'
-            }
+    new Block('title', 'Site Constructor on Vanilla JS', {
+        tag: 'h2',
+        styles: {
+            background: 'linear-gradient(to right, #ff0099, #493240)',
+            color: '#ffffff',
+            'text-align': 'center',
+            padding: '0.25rem'
         }
-    },
-    {
-        type: 'image', value: image, options: {
-            styles: {
-                padding: '2rem 0',
-                display: 'flex',
-                'justify-content': 'center'
-            },
-            imgStyles: {
-                width: '100%',
-                'max-width': '500px'
-            },
-            alt: 'cool photo'
+    }),
+    new Block('image', image, {
+        styles: {
+            padding: '2rem 0',
+            display: 'flex',
+            'justify-content': 'center'
+        },
+        imgStyles: {
+            width: '100%',
+            'max-width': '500px'
+        },
+        alt: 'cool photo'
+    }),
+    new Block('columns', [
+        'Приложение на чистом JavaScript, без использования библиотек',
+        'Узнаешь как работают принципы SOLID и ООП в JavaScript за один курс',
+        'JavaScript - это просто, интересно. Научись создавать любые UI своими руками'
+    ], {
+        styles: {
+            background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
+            padding: '2rem',
+            color: '#fff',
+            'font-weight': 'bold'
         }
-    },
-    {
-        type: 'columns', value: [
-            'Приложение на чистом JavaScript, без использования библиотек',
-            'Узнаешь как работают принципы SOLID и ООП в JavaScript за один курс',
-            'JavaScript - это просто, интересно. Научись создавать любые UI своими руками'
-        ], options: {
-            styles: {
-                background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
-                padding: '2rem',
-                color: '#fff',
-                'font-weight': 'bold'
-            }
+    }),
+    new Block('text', 'Тут ты найдешь исчерпывающую информацию по любым аспектам языка, любым фреймворкам, такие как: React, Vue, Angular, Node, Svelte, Express, Next, Nuxt и многое другое. Присоединяйся!', {
+        styles: {
+            background: 'linear-gradient(to left, #f2994a, #f2c94c)',
+            padding: '1rem',
+            'font-weight': 'bold'
         }
-    },
-    {
-        type: 'text',
-        value: 'Тут ты найдешь исчерпывающую информацию по любым аспектам языка, любым фреймворкам, такие как: React, Vue, Angular, Node, Svelte, Express, Next, Nuxt и многое другое. Присоединяйся!',
-        options: {
-            styles: {
-                background: 'linear-gradient(to left, #f2994a, #f2c94c)',
-                padding: '1rem',
-                'font-weight': 'bold'
-            }
-        }
-    },
-
+    }),
 ]
