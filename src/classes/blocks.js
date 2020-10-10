45abf7e39988abaf0ce7176b6cb1b9b2457a18f2
Roman Loiko <grevzi@gmail.com>
2020-10-10 12:57:45 +0300

    updates for blocks classes

--- src/classes/blocks.js
+++ src/classes/blocks.js
@@ -5,3 +5,27 @@ export class Block {
         this.options = options;
     }
 }
+
+export class TitleBlock extends Block {
+    constructor(value, options) {
+        super('title', value, options)
+    }
+}
+
+export class ImageBlock extends Block {
+    constructor(value, options) {
+        super('image', value, options)
+    }
+}
+
+export class ColumnsBlock extends Block {
+    constructor(value, options) {
+        super('columns', value, options)
+    }
+}
+
+export class TextBlock extends Block {
+    constructor(value, options) {
+        super('text', value, options)
+    }
+}
--- src/model.js
+++ src/model.js
@@ -1,7 +1,7 @@
 import image from './assets/image.png'
-import {Block} from './classes/blocks'
+import {TitleBlock, ImageBlock, ColumnsBlock, TextBlock} from './classes/blocks'
 export const model = [
-    new Block('title', 'Site Constructor on Vanilla JS', {
+    new TitleBlock('Site Constructor on Vanilla JS', {
         tag: 'h2',
         styles: {
             background: 'linear-gradient(to right, #ff0099, #493240)',
@@ -10,7 +10,7 @@ export const model = [
             padding: '0.25rem'
         }
     }),
-    new Block('image', image, {
+    new ImageBlock(image, {
         styles: {
             padding: '2rem 0',
             display: 'flex',
@@ -22,7 +22,7 @@ export const model = [
         },
         alt: 'cool photo'
     }),
-    new Block('columns', [
+    new ColumnsBlock([
         'Приложение на чистом JavaScript, без использования библиотек',
         'Узнаешь как работают принципы SOLID и ООП в JavaScript за один курс',
         'JavaScript - это просто, интересно. Научись создавать любые UI своими руками'
@@ -34,7 +34,7 @@ export const model = [
             'font-weight': 'bold'
         }
     }),
-    new Block('text', 'Тут ты найдешь исчерпывающую информацию по любым аспектам языка, любым фреймворкам, такие как: React, Vue, Angular, Node, Svelte, Express, Next, Nuxt и многое другое. Присоединяйся!', {
+    new TextBlock('Тут ты найдешь исчерпывающую информацию по любым аспектам языка, любым фреймворкам, такие как: React, Vue, Angular, Node, Svelte, Express, Next, Nuxt и многое другое. Присоединяйся!', {
         styles: {
             background: 'linear-gradient(to left, #f2994a, #f2c94c)',
             padding: '1rem',
