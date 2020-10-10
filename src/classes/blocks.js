263eb09ba64e4aad8637de7522d89a26ee81af81
Roman Loiko <grevzi@gmail.com>
2020-10-10 13:11:15 +0300

    Blocks Classes  - added toHtml method  - removed templates helper functions

--- src/classes/blocks.js
+++ src/classes/blocks.js
@@ -1,8 +1,14 @@
-export class Block {
+import {col, css, row} from "../utils";
+
+class Block {
     constructor(type, value, options) {
         this.type = type;
         this.value = value;
         this.options = options;
+    }
+
+    toHtml() {
+        throw new Error('You have to extend method [ toHtml ]')
     }
 }
 
@@ -10,11 +16,21 @@ export class TitleBlock extends Block {
     constructor(value, options) {
         super('title', value, options)
     }
+
+    toHtml() {
+        const {tag = 'h1', styles} = this.options;
+        return row(col(`<${tag}>${this.value}</${tag}>`), css(styles))
+    }
 }
 
 export class ImageBlock extends Block {
     constructor(value, options) {
         super('image', value, options)
+    }
+
+    toHtml() {
+        const {styles, imgStyles, alt} = this.options;
+        return row(`<img src="${this.value}" alt="${alt}" style="${css(imgStyles)}">`, css(styles));
     }
 }
 
@@ -22,10 +38,21 @@ export class ColumnsBlock extends Block {
     constructor(value, options) {
         super('columns', value, options)
     }
+
+    toHtml() {
+        const {styles} = this.options;
+        const columns = this.value.map(col).join('')
+        return row(columns, css(styles))
+    }
 }
 
 export class TextBlock extends Block {
     constructor(value, options) {
         super('text', value, options)
+    }
+
+    toHtml() {
+        const {styles} = this.options;
+        return row(col(`<p>${this.value}</p>`), css(styles))
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import {model} from './model'
-import {templates} from './templates'
 import './styles/main.css'
 
 const $site = document.querySelector('#site');
 
-model.forEach(block => {
-    if (templates.hasOwnProperty(block.type)) {
-        $site.insertAdjacentHTML('beforeend', templates[block.type](block))
-
-    }
-})
+model.forEach(block => $site.insertAdjacentHTML('beforeend', block.toHtml()))
